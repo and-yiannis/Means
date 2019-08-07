1691e370fd6e6ef4e6e65fd9eb45ac6d6726bdf7
Pants <pants@Pants.0.2.15>
2019-08-07 16:03:05 +0300

    Comments

---
+++ YelpCamp/v3/app.js
@@ -0,0 +1,70 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    Campground = require("./models/campground"),
+    seedDB = require("./seeds");
+
+mongoose.connect("mongodb://localhost:27017/yelp_camp_v3",
+                 { useNewUrlParser: true });
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+seedDB();
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res){
+    // Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else {
+             res.render("index", {campgrounds:allCampgrounds});
+        }
+    });
+});
+
+app.post("/campgrounds", function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc};
+    //Create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);            
+        } else {
+            //redirect back to campgrounds page
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new.ejs");
+});
+
+// SHOW - shows more info about one campground
+app.get("/campgrounds/:id", function(req, res){
+    Campground.findById(req.params.id).populate("comments").exec(
+        function(err, foundCampground){
+            if(err){
+                console.log(err);
+            } else {
+                console.log(foundCampground);
+                res.render("show", {campground: foundCampground});
+            }
+        }
+    );
+});
+
+app.listen(3000, 'localhost', function(){
+    console.log("The YelpCamp Has Started!");
+});
+
+
+
+
---
+++ YelpCamp/v3/seeds.js
@@ -0,0 +1,63 @@
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var Comment = require("./models/comment");
+
+var data = [
+    {
+        name: "Cloud's Rest",
+        image: "https://images.unsplash.com/photo-1537905569824-f89f14cceb68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        description: "Lorem ipsum dolor sit omet, consectetur adipis"
+    },
+    {
+        name: "Desert Mesa",
+        image: "https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        description: "Lorem ipsum dolor sit omet, consectetur adipis"
+    },
+    {
+        name: "Canyon Floor",
+        image: "https://images.unsplash.com/photo-1502218808493-e5fd26249efc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        description: "Lorem ipsum dolor sit omet, consectetur adipis"
+    }
+]
+
+function seedDB(){
+    Campground.deleteMany({}, function(err){
+        if(err){
+            console.log(err);
+        }
+        console.log("removed campgrounds!");
+        Comment.deleteMany({}, function(err) {
+            if(err){
+                console.log(err);
+            }
+            console.log("removed comments!");
+            //add a few campgrounds
+            data.forEach(function(seed){
+                Campground.create(seed, function(err, campground){
+                    if(err){
+                        console.log(err)
+                    } else {
+                        console.log("added a campground");
+                        //create a comment
+                        Comment.create({
+                            text: "This place is great, but I wish there was ..",
+                            author: "Homer"
+                        }, function(err, comment){
+                            if(err){
+                                console.log(err);
+                            } else {
+                                campground.comments.push(comment);
+                                campground.save();
+                                console.log("Created new comment");
+                            }
+                        });
+                    }
+                });
+            });
+        });
+    });
+    //add a few comments
+}
+
+module.exports = seedDB;
+
